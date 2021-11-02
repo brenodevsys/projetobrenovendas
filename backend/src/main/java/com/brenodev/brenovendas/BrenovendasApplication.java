c87ae46e7c86cf7cd083ba224f272066e6478c63
Breno Delgado <brenodelgado.dev@gmail.com>
2021-11-02 17:32:22 -0300

    Project Created

---
+++ backend/src/main/java/com/brenodev/brenovendas/BrenovendasApplication.java
@@ -0,0 +1,13 @@
+package com.brenodev.brenovendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrenovendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrenovendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/brenodev/brenovendas/BrenovendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brenodev.brenovendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrenovendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
