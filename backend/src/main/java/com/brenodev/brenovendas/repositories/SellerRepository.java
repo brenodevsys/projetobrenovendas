597a678e9313712ce3f3e1a86a6c6b2371948fc6
Breno Delgado <brenodelgado.dev@gmail.com>
2021-11-10 21:32:54 -0300

    Layers

---
+++ backend/src/main/java/com/brenodev/brenovendas/repositories/SellerRepository.java
@@ -0,0 +1,10 @@
+package com.brenodev.brenovendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.brenodev.brenovendas.entities.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller, Long>{
+
+	
+}
---
+++ backend/src/main/java/com/brenodev/brenovendas/services/SellerService.java
@@ -0,0 +1,27 @@
+package com.brenodev.brenovendas.services;
+
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.brenodev.brenovendas.dto.SellerDTO;
+import com.brenodev.brenovendas.entities.Seller;
+import com.brenodev.brenovendas.repositories.SellerRepository;
+
+@Service
+public class SellerService {
+	
+	@Autowired
+	private SellerRepository repository;
+	
+	public List<SellerDTO> findAll() {
+		List<Seller> result = repository.findAll();
+		return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList());
+		
+	}
+	
+
+}
